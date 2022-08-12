3e40c3e16be8b8f4c522b25d829e02fc33f633a5
Svetlana_Kovalevich <svetik14-06@mail.ru>
2022-08-12 10:02:10 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+function countPositivesSumNegatives(input) {
+    let positiveNums = 0;
+    let negativeNums = 0;
+    if (input === null || input.length === 0) {
+        return [];
+    } else {
+        input.map((num) => num > 0 ? positiveNums++ : negativeNums += num);
+    }
+    return [positiveNums , negativeNums];
+}
+console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]))
