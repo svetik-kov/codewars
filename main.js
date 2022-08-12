d4fac91e41d29e3d52642746a907e6dec453230f
Svetlana_Kovalevich <svetik14-06@mail.ru>
2022-08-12 10:15:12 +0300

    Given an array of integers.
    Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
    
    If the input is an empty array or is null, return an empty array.
    
    Example
    For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].

--- main.js
+++ main.js
@@ -8,4 +8,6 @@ function countPositivesSumNegatives(input) {
     }
     return [positiveNums , negativeNums];
 }
-console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]))
+//console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]))
+
+//Implement a function which multiplies two numbers.
