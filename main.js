46b09d97f60d7d77ad9339b0a6a20e2a2c3ccfe9
Svetlana_Kovalevich <svetik14-06@mail.ru>
2022-08-12 10:21:11 +0300

    Implement a function which multiplies two numbers.

--- main.js
+++ main.js
@@ -11,3 +11,7 @@ function countPositivesSumNegatives(input) {
 //console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]))
 
 //Implement a function which multiplies two numbers.
+function multiply(num1, num2) {
+
+    return num1*num2;
+}
