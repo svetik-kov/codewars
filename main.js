08f26c04e76a371fcc3ae75438e052d81c2a4b2b
Svetlana_Kovalevich <svetik14-06@mail.ru>
2022-08-19 21:20:28 +0300

    Given a non-empty array of integers, return the result of multiplying the values together in order. Example:  [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24

--- main.js
+++ main.js
@@ -91,4 +91,10 @@ function makeUpperCase(str) {
 }
 console.log(makeUpperCase('hello'))
 
+//Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
 //
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+function grow(x){
+    return x.reduce((acc,el)=>acc*el,1)
+}
+console.log(grow([1, 2, 3, 4]))
