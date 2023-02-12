80d920b2e6aa6c54b79a3cc5c9d992d5f2018129
Svetlana_Kovalevich <svetik14-06@mail.ru>
2023-02-12 20:03:02 +0300

    You are given an odd-length array of integers, in which all of them are the same, except for one single number.
        Complete the method which accepts such an array, and returns that single different number.
    
        The input array will always be valid! (odd-length >= 3)
    
    Examples
        [1, 1, 2] ==> 2
        [17, 17, 3, 17, 17, 17, 17] ==> 3*

--- main.js
+++ main.js
@@ -313,3 +313,25 @@ function repeatStr (n, s) {
 
     return  final;
 }
+
+
+/*
+You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+    Complete the method which accepts such an array, and returns that single different number.
+
+    The input array will always be valid! (odd-length >= 3)
+
+Examples
+    [1, 1, 2] ==> 2
+    [17, 17, 3, 17, 17, 17, 17] ==> 3*/
+
+function stray(numbers) {
+    let arr = numbers.sort();
+    if (arr[0] !== arr[1]) {
+        return arr[0];
+    } else {
+        return arr[arr.length - 1];
+    }
+
+}
