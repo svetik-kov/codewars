d5a37b5d741ddb4196848d7ea752083f480bb150
Svetlana_Kovalevich <svetik14-06@mail.ru>
2023-02-20 00:28:35 +0300

    Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
    For example,
    
        [true,  true,  true,  false,
            true,  true,  true,  true ,
            true,  false, true,  false,
            true,  false, false, true ,
            true,  true,  true,  true ,
            false, false, true,  true]
    The correct answer would be 17.
    
    Hint: Don't forget to check for bad values like null/undefined*

--- main.js
+++ main.js
@@ -364,3 +364,33 @@ var isSquare = function(n){
     {return true}
 
 }
+
+/*Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+For example,
+
+    [true,  true,  true,  false,
+        true,  true,  true,  true ,
+        true,  false, true,  false,
+        true,  false, false, true ,
+        true,  true,  true,  true ,
+        false, false, true,  true]
+The correct answer would be 17.
+
+Hint: Don't forget to check for bad values like null/undefined*/
+
+function countSheeps(arrayOfSheep) {
+    let countSheeps=0
+    for (let i=0;i<arrayOfSheep.length;i++){
+        if (arrayOfSheep[i]){
+            countSheeps++
+        }
+    }
+    return countSheeps
+}
+//2 способ
+function countSheeps(arrayOfSheep) {
+
+    return arrayOfSheep.filter((sheep)=>sheep===true).length
+}
+
