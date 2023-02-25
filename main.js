b37d8ac2336978d12ad19c852bc44f906d197524
Svetlana_Kovalevich <svetik14-06@mail.ru>
2023-02-25 22:49:43 +0300

    Count the number of Duplicates Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
        Example
    "abcde" -> 0 # no characters repeats more than once
    "aabbcde" -> 2 # 'a' and 'b'
    "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
    "indivisibility" -> 1 # 'i' occurs six times
    "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
    "aA11" -> 2 # 'a' and '1'
    "ABBA" -> 2 # 'A' and 'B' each occur twice*

--- main.js
+++ main.js
@@ -394,3 +394,32 @@ function countSheeps(arrayOfSheep) {
     return arrayOfSheep.filter((sheep)=>sheep===true).length
 }
 
+
+
+/*
+Count the number of Duplicates
+Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+    Example
+"abcde" -> 0 # no characters repeats more than once
+"aabbcde" -> 2 # 'a' and 'b'
+"aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+"indivisibility" -> 1 # 'i' occurs six times
+"Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+"aA11" -> 2 # 'a' and '1'
+"ABBA" -> 2 # 'A' and 'B' each occur twice*/
+function duplicateCount(text){
+
+    let newText=text.toLowerCase()
+    let dublicate={}
+    let counter=0
+    for (let i=0;i<newText.length;i++){
+        if(!dublicate[newText[i]]){
+            dublicate[newText[i]]=1
+        } else if(dublicate[newText[i]]<2){
+            dublicate[newText[i]]+=1
+            counter++
+        }
+    }
+    return counter
+}
