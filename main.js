0c231ae3477c2b785f52025602e1a1cf2257d253
Svetlana_Kovalevich <svetik14-06@mail.ru>
2023-02-26 23:23:31 +0300

    Implement a function which convert the given boolean value into its string representation.
        Note: Only valid inputs will be given

--- main.js
+++ main.js
@@ -423,3 +423,10 @@ function duplicateCount(text){
     }
     return counter
 }
+/*
+Implement a function which convert the given boolean value into its string representation.
+
+    Note: Only valid inputs will be given.*/
+function booleanToString(b){
+    return b.toString()
+}
