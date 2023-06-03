28d37a62443e43d4f77304c11d5b4e77b871084e
Svetlana_Kovalevich <svetik14-06@mail.ru>
2023-06-03 19:00:01 +0300

    //Convert number to reversed array of digits // Given a random non-negative number, you have to return the digits of this number within an array in reverse order. // // Example(Input => Output): // 35231 => [1,3,2,5,3] // 0 => [0]

--- main.js
+++ main.js
@@ -709,3 +709,13 @@ function century(year) {
     }
     return century;
 }
+//Convert number to reversed array of digits
+// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+//
+// Example(Input => Output):
+// 35231 => [1,3,2,5,3]
+// 0 => [0]
+function digitize(n) {
+    arr= String(n).split('').reverse().map(Number)
+    return arr
+}
