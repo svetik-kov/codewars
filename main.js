c05cef8a8b935da21ff25b1fe298a0b11f3e7444
Svetlana_Kovalevich <svetik14-06@mail.ru>
2023-06-03 18:02:49 +0300

    Very simple, given an integer or a floating-point number, find its opposite.
        Examples:
    
    1: -1
    14: -14
    -34: 34

--- main.js
+++ main.js
@@ -618,3 +618,17 @@ Complete the solution so that it reverses the string passed into it.
 function solution(str){
     return str.split('').reverse().join('')
 }
+
+
+
+/*
+Very simple, given an integer or a floating-point number, find its opposite.
+
+    Examples:
+
+1: -1
+14: -14
+-34: 34*/
+function opposite(number) {
+    return -number
+}
