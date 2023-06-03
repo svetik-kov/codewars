99d6fc07d77abc420f9268685198a227657b3b7c
Svetlana_Kovalevich <svetik14-06@mail.ru>
2023-06-03 17:43:30 +0300

    Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers

--- main.js
+++ main.js
@@ -602,3 +602,9 @@ function makeNegative(num) {
     }
 }
 
+/*Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.*/
+
+function evenOrOdd(number) {
+    return (number%2===0)?'Even':'Odd'
+
+}
