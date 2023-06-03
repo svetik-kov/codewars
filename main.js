c31cb44f34e0e9fb10c3263b87fc8dfd9817c4ce
Svetlana_Kovalevich <svetik14-06@mail.ru>
2023-06-03 17:05:47 +0300

    You get an array of numbers, return the sum of all of the positives ones.
        Example [1,-4,7,12] => 1 + 7 + 12 = 20
    
    Note: if there is nothing to sum, the sum is default to 0

--- main.js
+++ main.js
@@ -541,3 +541,32 @@ function getAge(inputString){
 function getAge1(inputString){
     return parseInt(inputString)
 }
+
+
+
+/*You get an array of numbers, return the sum of all of the positives ones.
+
+    Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.*/
+function positiveSum(arr) {
+    let sum=0
+    for(let i=0;i<arr.length;i++){
+        if (arr[i]>0)
+            sum+=arr[i]
+    }
+    return sum
+}
+//2 способ
+function positiveSum(arr) {
+    return arr.reduce((a,b)=> a + (b > 0 ? b : 0),0);
+}
+
+//3 способ
+function positiveSum(arr) {
+    return arr.filter(x => x > 0).reduce((a, b) => a+b, 0);
+}
+//4 способ
+function positiveSum( obj ) {
+    return obj.map(el => el < 0 ? 0 : el).reduce((acc, i)=> acc + i, 0)
+}
