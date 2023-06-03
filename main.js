79baafe15dfdb28c71cd4d94890e140e5ac8e081
Svetlana_Kovalevich <svetik14-06@mail.ru>
2023-06-03 17:53:45 +0300

    Complete the solution so that it reverses the string passed into it.
    'world'  =>  'dlrow'
    'word'   =>  'drow'

--- main.js
+++ main.js
@@ -608,3 +608,13 @@ function evenOrOdd(number) {
     return (number%2===0)?'Even':'Odd'
 
 }
+
+
+/*
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'*/
+function solution(str){
+    return str.split('').reverse().join('')
+}
