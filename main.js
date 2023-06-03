76ac27066a7fc0c184779eaf4260f9955c3f4869
Svetlana_Kovalevich <svetik14-06@mail.ru>
2023-06-03 18:31:43 +0300

    DESCRIPTION: //     Introduction // The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc. // //     Task // Given a year, return the century it is in. // // Examples // 1705 --> 18 // 1900 --> 19 // 1601 --> 17 // 2000 --> 20 // Note: this kata uses strict construction as shown in the description and the examples, you can read more about it here

--- main.js
+++ main.js
@@ -674,3 +674,38 @@ function litres(time) {
 
     return Math.floor(time/2);
 }
+
+// DESCRIPTION:
+//     Introduction
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+//
+//     Task
+// Given a year, return the century it is in.
+//
+// Examples
+// 1705 --> 18
+// 1900 --> 19
+// 1601 --> 17
+// 2000 --> 20
+// Note: this kata uses strict construction as shown in the description and the examples, you can read more about it here
+function century(year) {
+    let centuryCount = 0;
+    while (year > 0){
+        year = year - 100;
+        centuryCount = centuryCount + 1;
+    }
+    return centuryCount;
+}
+//2 способ
+const century = year => Math.ceil(year/100)
+//3 способ
+function century(year) {
+    var century = 0;
+
+    for(var i = 0; i < year; i++) {
+        if(i % 100 == 0) {
+            century++;
+        }
+    }
+    return century;
+}
