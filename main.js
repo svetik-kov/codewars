dfdead2c9bb062413cd162700e4dcfa5a48936c4
Svetlana_Kovalevich <svetik14-06@mail.ru>
2023-06-04 07:58:59 +0300

    Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
    [Make sure you type the exact thing I wrote or the program may not execute properly]

--- main.js
+++ main.js
@@ -755,3 +755,16 @@ function isDivisible(n, x, y) {
 //console.log(isDivisible(6,2,7))
 
 
+//Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+//
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+function greet(name){
+    return `Hello, ${name} how are you doing today?`
+}
+
+console.log(greet("Sveta"))
+
+
+
+
