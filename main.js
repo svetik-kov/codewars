604be52314bb2112c3872ba7d7b8432da7da9e5a
Svetlana_Kovalevich <svetik14-06@mail.ru>
2023-08-07 19:19:10 +0300

    Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty ( zero length ).
    Hint for R users:
    
    The length of string is not always the same as the number of characters
    For example: (Input1, Input2) --> output
    
    ("1", "22") --> "1221"
    ("22", "1") --> "1221"

--- main.js
+++ main.js
@@ -797,4 +797,24 @@ function lovefunc(flower1, flower2){
     }
 }
 
-console.log(lovefunc(586,777))
+//console.log(lovefunc(586,777))
+
+
+//Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty ( zero length ).
+//
+// Hint for R users:
+//
+// The length of string is not always the same as the number of characters
+// For example: (Input1, Input2) --> output
+//
+// ("1", "22") --> "1221"
+// ("22", "1") --> "1221"
+function solution(a, b){
+    // your code here
+    if (a.length>b.length){
+        //let c = b+a+b
+        return b.concat(a,b)
+    }
+    return a.concat(b,a)
+        }
+console.log(solution("1", "22"))
