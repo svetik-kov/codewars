0abdb580b0bb2a22d5a467792a798c0c574c14db
Svetlana_Kovalevich <svetik14-06@mail.ru>
2023-08-27 20:57:00 +0300

    //In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out. // // Example // filter_list([1,2,'a','b']) == [1,2] // filter_list([1,'a','b',0,15]) == [1,0,15] // filter_list([1,2,'aasf','1','123',123]) == [1,2,123]

--- main.js
+++ main.js
@@ -908,7 +908,22 @@ function stringy(size) {
     }
     return str
 }
-console.log(stringy(6))
+//console.log(stringy(6))
 
 //2 способ
 const stringy1 = size => "10".repeat(size).slice(0,size);
+
+//In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+//
+// Example
+// filter_list([1,2,'a','b']) == [1,2]
+// filter_list([1,'a','b',0,15]) == [1,0,15]
+// filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+function filter_list(l) {
+    // Return a new array with the strings filtered out
+    return l.filter(el=> typeof el!=='string')
+
+}
+
+console.log(filter_list([1,'a','b',0,15]))
