e4ec9e3009bc7f50bc963e2bedf5fcd782151473
Svetlana_Kovalevich <svetik14-06@mail.ru>
2023-08-31 21:08:58 +0300

    In this Kata we are passing a number (n) into a function. // // Your code will determine if the number passed is even (or not). // // The function needs to return either a true or false. // // Numbers may be positive or negative, integers or floats. // // Floats with decimal part non equal to zero are considered UNeven for this kata.

--- main.js
+++ main.js
@@ -1003,4 +1003,19 @@ function billboard(name, price = 30) {
 function billboard1(name, price = 30) {
     return name.split('').reduce((sum, letter) => sum + price, 0);
 }
-console.log(billboard("Hadufuns John", 20))
+//console.log(billboard("Hadufuns John", 20))
+
+//In this Kata we are passing a number (n) into a function.
+//
+// Your code will determine if the number passed is even (or not).
+//
+// The function needs to return either a true or false.
+//
+// Numbers may be positive or negative, integers or floats.
+//
+// Floats with decimal part non equal to zero are considered UNeven for this kata.
+function testEven(n) {
+  return  n%2===0? true:false
+}
+
+console.log(testEven(n))
