61423d165fb1ef9eca354986a19dd91d06fed320
Svetlana_Kovalevich <svetik14-06@mail.ru>
2023-09-01 19:17:19 +0300

    Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
    Example:["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
     None of the arrays will be empty, so you don't have to worry about that!

--- main.js
+++ main.js
@@ -1117,4 +1117,15 @@ function greet(name){
     //return "Hello, " + (name == "Johnny" ? "my love" : name) + "!";
 }
 
-console.log(greet("Johnny"))
+//console.log(greet("Johnny"))
+
+//Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+//
+// Example:
+// ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+//
+// None of the arrays will be empty, so you don't have to worry about that!
+function removeEveryOther(arr){
+   return arr.filter((el,index)=>index%2===0)
+}
+console.log(removeEveryOther(['Hello', 'Goodbye', 'Hello Again']))
