11ce81324646c19c55d63fab091dea11c4752373
Svetlana_Kovalevich <svetik14-06@mail.ru>
2023-09-01 15:35:44 +0300

    Create a method that takes as input a name, city, and state to welcome a person. Note that name will be an array consisting of one or more values that should be joined together with one space between each, and the length of the name array in test cases will vary.
     Example:
    
     ['John', 'Smith'], 'Phoenix', 'Arizona'
     This example will return the string Hello, John Smith! Welcome to Phoenix, Arizona!

--- main.js
+++ main.js
@@ -1059,4 +1059,17 @@ if (sperm==='XX') {
 
 }
 
-console.log(chromosomeCheck('XY'))
+//console.log(chromosomeCheck('XY'))
+
+//Create a method that takes as input a name, city, and state to welcome a person. Note that name will be an array consisting of one or more values that should be joined together with one space between each, and the length of the name array in test cases will vary.
+//
+// Example:
+//
+// ['John', 'Smith'], 'Phoenix', 'Arizona'
+// This example will return the string Hello, John Smith! Welcome to Phoenix, Arizona!
+function sayHello( name, city, state ) {
+    //return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`
+    return `Hello, ${name.map((el)=>el).join(' ')}! Welcome to ${city}, ${state}!`
+}
+
+console.log(sayHello(['John', 'Smith'], 'Phoenix', 'Arizona'))
