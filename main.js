6bbe46d97bb317965d2ea11ffaa3044bf532aefe
Svetlana_Kovalevich <svetik14-06@mail.ru>
2023-09-04 12:03:12 +0300

    Issue  Looks like some hoodlum plumber and his brother has been running around and damaging your stages again.  The pipes connecting your level's stages together need to be fixed before you receive any more complaints.  The pipes are correct when each pipe after the first is 1 more than the previous one.
     Task
     Given a list of unique numbers sorted in ascending order, return a new list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included).
    
     Example
     Input:  1,3,5,6,7,8 Output: 1,2,3,4,5,6,7,8

--- main.js
+++ main.js
@@ -13,7 +13,6 @@ function countPositivesSumNegatives(input) {
 
 //Implement a function which multiplies two numbers.
 function multiply(num1, num2) {
-
     return num1 * num2;
 }
 
@@ -1181,4 +1180,34 @@ function findMultiples(integer, limit) {
    }
    return result
 }
-console.log(findMultiples(5, 25))
+//console.log(findMultiples(5, 25))
+
+//Issue
+// Looks like some hoodlum plumber and his brother has been running around and damaging your stages again.
+//
+// The pipes connecting your level's stages together need to be fixed before you receive any more complaints.
+//
+// The pipes are correct when each pipe after the first is 1 more than the previous one.
+//
+// Task
+// Given a list of unique numbers sorted in ascending order, return a new list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included).
+//
+// Example
+// Input:  1,3,5,6,7,8 Output: 1,2,3,4,5,6,7,8
+
+function pipeFix1(numbers){
+ return  Array.from({length:numbers.slice(-1)-numbers[0]+1},
+    (_,index)=>index+numbers[0])
+ }
+function pipeFix(numbers){
+   let first = numbers[0];
+    let last = numbers[numbers.length-1];
+
+    let  arr = [];
+    for(let i = first; i <= last; i++) {
+        arr.push(i);
+    }
+    return arr;
+}
+// assert.deepEqual(pipeFix([1,2,3,5,6,8,9]),[1,2,3,4,5,6,7,8,9]);
+console.log(pipeFix([1,2,3,5,6,8,9]))
