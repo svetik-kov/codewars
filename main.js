661e01923dd54145a5270e9fe5548e0ad9eb8e66
Svetlana_Kovalevich <svetik14-06@mail.ru>
2023-10-16 17:03:50 +0300

    The function is not returning the correct values. Can you figure out why?  Example (Input --> Output ): 3 --> "Earth"

--- main.js
+++ main.js
@@ -1431,3 +1431,35 @@ function neutralise(s1, s2) {
 
 console.log(neutralise("--++--", "++--++")) //000000
 console.log(neutralise("-++-", "-+-+")) //-+00
+
+
+//The function is not returning the correct values. Can you figure out why?
+//
+// Example (Input --> Output ):
+//
+// 3 --> "Earth"
+function getPlanetName(id){
+    var name;
+    switch(id){
+        case 1:
+        return    name = 'Mercury'
+        case 2:
+        return     name = 'Venus'
+        case 3:
+         return   name = 'Earth'
+        case 4:
+        return     name = 'Mars'
+        case 5:
+         return   name = 'Jupiter'
+        case 6:
+          return  name = 'Saturn'
+        case 7:
+         return   name = 'Uranus'
+        case 8:
+         return   name = 'Neptune'
+        default:
+            return name;
+    }
+}
+
+console.log(getPlanetName(2))
