929da7901a3e781cf43da953edf033e59a89bb5f
Svetlana_Kovalevich <svetik14-06@mail.ru>
2023-10-16 17:54:00 +0300

    Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers

--- main.js
+++ main.js
@@ -1462,4 +1462,14 @@ function getPlanetName(id){
     }
 }
 
-console.log(getPlanetName(2))
+//console.log(getPlanetName(2))
+
+//Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function evenOrOdd(number) {
+    if (number %2===0){
+        return "Even"
+    } else {
+        return "Odd"
+    }
+}
