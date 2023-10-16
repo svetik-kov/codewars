95795e1f0e3c2a961023b24dd2e26c7fb462311a
Svetlana_Kovalevich <svetik14-06@mail.ru>
2023-10-16 21:36:41 +0300

    Write a function that calculates the original product price, without VAT.
    Example
    If a product price is 200.00 and VAT is 15%, then the final product price (with VAT) is: 200.00 + 15% = 230.00
    
    Thus, if your function receives 230.00 as input, it should return 200.00
    
    Notes:
    
    VAT is always 15% for the purposes of this Kata.
    Round the result to 2 decimal places.
    If null value given then return -1

--- main.js
+++ main.js
@@ -1515,3 +1515,26 @@ function whoseMove(lastPlayer, win) {
     const opp = lastPlayer === 'black' ? 'white' : 'black';
     return  win ? lastPlayer : opp;
 }
+
+//Write a function that calculates the original product price, without VAT.
+//
+// Example
+// If a product price is 200.00 and VAT is 15%, then the final product price (with VAT) is: 200.00 + 15% = 230.00
+//
+// Thus, if your function receives 230.00 as input, it should return 200.00
+//
+// Notes:
+//
+// VAT is always 15% for the purposes of this Kata.
+// Round the result to 2 decimal places.
+// If null value given then return -1
+
+function excludingVatPrice(price){
+    if (price===null){
+        return -1;
+    } else{
+        res=+(price*100/115)
+        return +(res.toFixed(2))
+    }
+
+}
