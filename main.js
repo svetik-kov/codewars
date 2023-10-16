ab4678085e521f137505572dba73d83124cbf2b6
Svetlana_Kovalevich <svetik14-06@mail.ru>
2023-10-16 16:55:17 +0300

    Given two strings comprised of + and -, return a new string which shows how the two strings interact in the following way:
    When positives and positives interact, they remain positive.
    When negatives and negatives interact, they remain negative.
    But when negatives and positives interact, they become neutral, and are shown as the number 0.
    Worked Example
    ("+-+", "+--") ➞ "+-0"
    # Compare the first characters of each string, then the next in turn.
    # "+" against a "+" returns another "+".
    # "-" against a "-" returns another "-".
    # "+" against a "-" returns "0".
    # Return the string of characters.

--- main.js
+++ main.js
@@ -1388,4 +1388,46 @@ function hotpo(n) {
 
     return count;
 }
-console.log(hotpo(5))
+
+
+//console.log(hotpo(5))
+
+//Given two strings comprised of + and -, return a new string which shows how the two strings interact in the following way:
+//
+// When positives and positives interact, they remain positive.
+// When negatives and negatives interact, they remain negative.
+// But when negatives and positives interact, they become neutral, and are shown as the number 0.
+// Worked Example
+// ("+-+", "+--") ➞ "+-0"
+// # Compare the first characters of each string, then the next in turn.
+// # "+" against a "+" returns another "+".
+// # "-" against a "-" returns another "-".
+// # "+" against a "-" returns "0".
+// # Return the string of characters.
+// Examples
+// ("--++--", "++--++") ➞ "000000"
+//
+// ("-+-+-+", "-+-+-+") ➞ "-+-+-+"
+//
+// ("-++-", "-+-+") ➞ "-+00"
+// Notes
+// The two strings will be the same length
+
+function neutralise(s1, s2) {
+    let result = ''
+    for (let i = 0; i < s1.length; i++) {
+        const st1 = s1[i]
+        const st2 = s2[i]
+        if (st1 === "+" && st2 === '+') {
+            result += "+"
+        }else  if (st1 === "-" && st2 === '-') {
+                result += "-"
+        } else {
+            result += "0"
+        }
+    }
+    return result
+}
+
+console.log(neutralise("--++--", "++--++")) //000000
+console.log(neutralise("-++-", "-+-+")) //-+00
