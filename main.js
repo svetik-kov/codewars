89f06a10b4875d7813b4373c1b9cef295955c158
Svetlana_Kovalevich <svetik14-06@mail.ru>
2023-12-12 22:32:14 +0300

    Return the number (count) of vowels in the given string.  We will consider a, e, i, o, u as vowels for this Kata (but not y).  The input string will only consist of lower case letters and/or spaces.

--- main.js
+++ main.js
@@ -1705,7 +1705,24 @@ function reverseWords(str) {
 
 String.prototype.toJadenCase = function () {
     return this.split(' ').map(item => item[0].toUpperCase() + item.slice(1)).join(' ')
-    }).join(' ');
+
 };
 
+//Return the number (count) of vowels in the given string.
 //
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+//
+// The input string will only consist of lower case letters and/or spaces.
+
+function getCount(str) {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    let count = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        if (vowels.includes(str[i])) {
+            count++;
+        }
+    }
+
+    return count;
+}
