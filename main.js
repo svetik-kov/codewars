219681dae069471836b0d052a1e593eddd6682b6
Svetlana_Kovalevich <svetik14-06@mail.ru>
2023-12-12 19:10:34 +0300

    Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below. // // Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them. // // Example: // // Not Jaden-Cased: "How can mirrors be real if our eyes aren't real" // Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"

--- main.js
+++ main.js
@@ -1704,7 +1704,8 @@ function reverseWords(str) {
 
 
 String.prototype.toJadenCase = function () {
-    return this.split(' ').map(function(word) {
-        return word.charAt(0).toUpperCase() + word.slice(1);
+    return this.split(' ').map(item => item[0].toUpperCase() + item.slice(1)).join(' ')
     }).join(' ');
 };
+
+//
