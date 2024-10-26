659a01e5123535f7bdeac47d8c979cdc7c6d80a0
Svetlana_Kovalevich <svetik14-06@mail.ru>
2024-10-26 07:48:43 +0300

    The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
     Task
     Given a year, return the century it is in.

--- main.js
+++ main.js
@@ -1733,3 +1733,11 @@ function removeExclamationMarks(s) {
 
     return s.split().filter((el)=>el!=='!').join()
 }
+//Introduction
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+//
+// Task
+// Given a year, return the century it is in.
+function century(year) {
+    return Math.ceil(year/100)  ;
+}
